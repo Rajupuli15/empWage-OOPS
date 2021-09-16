357126e8a6a61a4c3cf0f9dec1159cf3db3f7f0e
Rajupuli15 <Rajupuli15@gmail.com>
2021-09-16 00:50:33 -0400

    Added:Attendance

--- src/com/bridgelabs/empWageOOPS.java
+++ src/com/bridgelabs/empWageOOPS.java
@@ -3,7 +3,21 @@ package com.bridgelabs;
 public class empWageOOPS {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to empWageOOPS");
+
+        // consonants
+
+        int IS_FULL_TIME = 1;
+
+        //computation
+
+        double empCheck = Math.floor(Math.random() * 10 ) * 2;
+        if (empCheck == IS_FULL_TIME)
+
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
+
+        // System.out.println("Welcome to empWageOOPS");
     }
 
 }
