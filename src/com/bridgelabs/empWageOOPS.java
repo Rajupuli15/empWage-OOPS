7603bdd3cc8cc6a6c24a9942793bfedc492c18e2
Rajupuli15 <Rajupuli15@gmail.com>
2021-09-16 01:02:40 -0400

    Added:empWage

--- src/com/bridgelabs/empWageOOPS.java
+++ src/com/bridgelabs/empWageOOPS.java
@@ -4,20 +4,26 @@ public class empWageOOPS {
 
     public static void main(String[] args) {
 
-        // consonants
+        // constants
 
         int IS_FULL_TIME = 1;
+        int WAGE_PER_HOUR =20;
+        int empHour = 0;
+        int Result = 0;
 
         //computation
 
         double empCheck = Math.floor(Math.random() * 10 ) * 2;
         if (empCheck == IS_FULL_TIME)
-
+        {
             System.out.println("Employee is Present");
-        else
+            empHour = 8; }
+        else {
             System.out.println("Employee is Absent");
+            empHour = 0; }
+        Result = empHour*WAGE_PER_HOUR;
+        System.out.println(Result);
 
-        // System.out.println("Welcome to empWageOOPS");
     }
 
 }
